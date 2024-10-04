83a96e96e6d6cabf741c5f9869e2c3238914430c
chitishviligabrieli <157848399+chitishviligabrieli@users.noreply.github.com>
2024-10-04 19:20:30 +0400

    asdasd

--- middleware.ts
+++ middleware.ts
@@ -44,5 +44,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-    matcher: ["/((?!api|_next/static|_next/public|icons|images|favicon.ico|.*\\.svg).*)"]
+    matcher: ["/((?!api|_next/static|_next/Public|icons|images|favicon.ico|.*\\.svg).*)"]
 };
